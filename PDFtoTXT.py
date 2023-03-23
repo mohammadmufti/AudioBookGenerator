2035bedc75c0e967a4b418e0fa2356ed02aff596
mohammadmufti <87641912+mohammadmufti@users.noreply.github.com>
2023-03-22 23:45:29 -0700

    Update PDFtoTXT.py
    Updating code to reflect changes as of PyPDF2 3.0...

--- PDFtoTXT.py
+++ PDFtoTXT.py
@@ -2,11 +2,11 @@ import PyPDF2
 
 def convert_pdf_to_text(pdf_file_path):
     with open(pdf_file_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
         text = ''
-        for page_num in range(pdf_reader.numPages):
-            page = pdf_reader.getPage(page_num)
-            text += page.extractText()
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
         return text
 
 pdf_file_path = input('Enter the name of the PDF file: ')
@@ -18,7 +18,7 @@ else:
 
 text = convert_pdf_to_text(pdf_file_path)
 
-with open(txt_file_path, 'w') as txt_file:
+with open(txt_file_path, 'w', encoding="utf-8") as txt_file:
     txt_file.write(text)
 
 print(f'Text extracted from {pdf_file_path} and saved to {txt_file_path}.')
