84610ae6ec6795063de70c248c38f549d596b06f
mohammadmufti <87641912+mohammadmufti@users.noreply.github.com>
2023-03-23 00:41:14 -0700

    Update PDFtoTXT.py
    Small modification to eliminate post conversion unnecessary white spaces and line breaks (vs preprocessing during conversion)

--- PDFtoTXT.py
+++ PDFtoTXT.py
@@ -17,6 +17,8 @@ else:
     txt_file_path = pdf_file_path + '.txt'
 
 text = convert_pdf_to_text(pdf_file_path)
+text = " ".join(text.split())
+print(text) # for testing
 
 with open(txt_file_path, 'w', encoding="utf-8") as txt_file:
     txt_file.write(text)
